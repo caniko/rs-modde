675055c121621ddd91e5ed9c07e3b876b7212e08
forgejo-actions <forgejo-actions@noreply.codeberg.org>
2026-07-10 09:33:11 +0000

    Deploy site 2026-07-10T09:33:04Z

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-313809ce.js',
+                path_to_root + 'searchindex-7fd5fe23.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
