e031e940fb70768c07e5cb028c6577b39467cb5d
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-10 19:00:06 +0200

    chore: adopt simit 0.17.3 release workflows

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
 
     simit = {
-      url = "git+https://codeberg.org/caniko/simit?ref=ci-release-publisher-maturation";
+      url = "git+https://codeberg.org/caniko/simit?ref=refs/tags/0.17.3";
       inputs.rs-harbor.follows = "rs-harbor";
       inputs.nixpkgs.follows = "rs-harbor/nixpkgs";
       inputs.rust-overlay.follows = "rs-harbor/rust-overlay";
